99ecc2e32f5be63a23271ef5de993dbef4b78392
Pranav794 <pranav_794@yahoo.com>
2014-02-03 20:25:55 -0500

    update2

--- tokenizer.c
+++ tokenizer.c
@@ -332,7 +332,7 @@ int main(int argc, char **argv) {
 		char *token = TKGetNextToken(start);
 				if(token == NULL)
 				{
-					continue;
+					break;
 				}
 		token = checkEscChar(token);
 		printf("%s\n", token);
